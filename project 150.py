bcdb0a0e03677d72fe7d25fe572c09f5ade8467b
alainakhan1234 <103361607+alainakhan1234@users.noreply.github.com>
2022-06-22 16:08:16 +0100

    Add files via upload

---
+++ project 150.py
@@ -0,0 +1,19 @@
+Country Name = ("India","Netherland","Italy")
+
+
+City Name = (Delhi, Rotterdam, Milan)
+
+max_profit = max(profits)
+max_profit_index = profits.index(max_profit)
+print(max_profit_index)
+
+display_country_list["text"] = "Country Name :" + str(country_list)
+
+min_profit = min(profits)
+min_profit_index = profits.index(min_profit)
+print(min_profit_index)
+
+
+display_city_list["text"] = "City Name :" + str(city_list)
+
+
